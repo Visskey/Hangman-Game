200abd502b1a90530759a58dc97d05f6a6ad3906
visarahmeti <58608274+Visskey@users.noreply.github.com>
2022-07-30 13:42:40 +0200

    Update Hangman.java

--- Hangman.java
+++ Hangman.java
@@ -43,10 +43,61 @@ class Game {
                 System.out.println("Ju keni " + temp + " jeta te mbetura");
             }
             System.out.print("Shkronja juaj:");
+            
+            guess = input.nextLine(); //pergjigja e lojtarit
+            guess = guess.toUpperCase();
+
+            letter = guess.charAt(0); // marrim vetem shkronjen e pare
+
+            guessescontainsguess = (guesses.indexOf(letter)) != -1;
+            guesses += letter;
+
+
+            letter = Character.toUpperCase(letter);
+            System.out.println();
+
+            if (guessescontainsguess == true) {
+                System.out.println("Ju e keni zgjedhur me pare shkronjen: " + letter + ". \n");
+            }
+
+            guessinword = (word.indexOf(letter)) != -1;
+
+
+            if (guessinword == true) {
+                
+                System.out.println(letter + " eshte ne kete fjale");
+                System.out.print("\n");
+
+                //zevendesojme _ me shkronjen nese ajo eshte e sakte
+                for (int position = 0; position < word.length(); position++) {
+
+                    if (word.charAt(position) == letter && word1.charAt(position) != letter) {
+
+                        word1 = word1.replaceAll("_ ", "_");
+                        String word2;
+                        word2 = word1.substring(0, position) + letter + word1.substring(position+ 1);
+                        word2 = word2.replaceAll("_", "_ ");
+                        word1 = word2;
+                    }
+                }
+            }
+            else {
+                System.out.println(letter + " nuk eshte ne kete fjale.");
+                wrong++;
+            }
+            guess_++;
+        }
+
+        if (wrong == 5) {
+            System.out.println("Ju humbet");
+        }
+        else {
+            System.out.print("Fjala eshte: " + word1 + "\n Well Played, you did it!!");
+        }
     }
         
 public static void main(String[] args)
     {
-        
+        hangman();
     }
 }
